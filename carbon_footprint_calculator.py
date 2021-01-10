83781af43eacb7b071f8e18ffed5f145c81e4ab9
stevenf7 <stevenfeng7@gmail.com>
2021-01-09 18:03:59 -0700

    carbon footprint calculation complete

--- carbon_footprint_calculator.py
+++ carbon_footprint_calculator.py
@@ -1,18 +1,45 @@
 import urllib.request
 import json
-import googlemaps
+import re
 
 api_key_google= 'AIzaSyCXkcgLEtAP8pKulgO3SGqotjR9wd8bNaY'
 
+#shipping pollution
+#source: https://www.sourcinghub.io/air-freight-vs-sea-freight-carbon-footprint/#:~:text=air%20shipping%20carbon%20footprint%20%E2%80%93%20which,grams%20of%20CO2%20per%20kilometer.
+
+AirFreight = 0.0005
+Truck = 0.00011
+Train = 0.00007
+
+
+
 ## Weight, category, Zip1,Zip2
-## Category: one of 9 category will have different carbon footprint per g
-## Distance: distance from manufacture to consumer
-## weight --> if doesn't exist, use default
-def carbon_footprint(category, distance):
+## Category: Amazon category emission, kg carbon co2 per kg product weight (in float)
+## Zip1, zip2, zip code of the orgin and destination (in string)
+## weight --> weight of the product, either in g, or kg (in string)
 
+def carbon_footprint(IsPrime, category, zip1, zip2, weight):
+    distance = distance_calculator(zip1, zip2)
+    if (weight != None):
+        weight = weight_calculator(weight)
+    else:
+        weight = 1 #assume weight is 1kg
 
+    #get product category
+    product_emission = weight * float(category)
+    shipping_emission = 0
+    if (distance == None):
+        distance = 1000
 
-    pass
+    if (distance < 100): #less than 100 km
+        shipping_emission = weight * Truck * distance
+    elif (IsPrime):
+        shipping_emission = weight * (Truck * distance * 0.2 + AirFreight * distance * 0.8)
+    else:
+        shipping_emission = weight * (Truck * distance * 0.2 + Train * distance * 0.8)
+
+    print("Shipping emission %f, Product emission %f" % (shipping_emission, product_emission))
+    return shipping_emission + product_emission
 
 def distance_calculator(zip1, zip2):
     zip1 = zip1.replace(' ','')
@@ -24,17 +51,29 @@ def distance_calculator(zip1, zip2):
         result = data["rows"][0]["elements"][0]["distance"]
         #print(data)
         distance = float(result['text'].replace(',','')[:-3])
-        print(distance)
+        #print(distance)
     except:
         #default distance to 1000
         print("Invalid zipcodes, using default distance: 1000km")
         distance = 1000.0
     return distance
 
+#takes in weight of the product in g, gram, kg, kilogram
+#returns weight in kg
+def weight_calculator(weight):
+    x = weight.split(' ')
+    num = float(x[0])
+    unit = x[1]
+    patternKG = '(?:kg|kilo)'
+    if(len(re.findall(patternKG, weight))==0): #in grams
+        num /=1000
+    return num
 
 
 if __name__ =='__main__':
     zipcode1 = '91748'
     zipcode2 = 'L4C0P8'
-    distance = distance_calculator(zipcode1, zipcode2)
-    str = '2,085 km'
+    weight = '12 kg'
+    category = 0.1
+    isPrime = False
+    print(carbon_footprint(isPrime,category,zipcode1, zipcode2, weight))
