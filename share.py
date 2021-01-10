fbe24476e5a0f57f38eddf0b773d463f6726c558
stevenf7 <34409734+stevenf7@users.noreply.github.com>
2021-01-10 00:39:24 -0700

    remove print statements

--- share.py
+++ share.py
@@ -8,13 +8,13 @@ def share(site, subject, message, link):
     url = None
     if (site == 'twitter'):
         url = 'https://twitter.com/intent/tweet?text='+urllib.parse.quote(message)+'+'+urllib.parse.quote(link)
-        #print(url)
+
     elif(site == 'facebook'):
         url = 'http://www.facebook.com/sharer.php?src=sp&u='+urllib.parse.quote(link)
-        #print(url)
+
     elif(site == 'email'):
         url = 'mailto:?subject=' + urllib.parse.quote(subject)+ '&body='+urllib.parse.quote(message)+"%0A%0A"+urllib.parse.quote(link)
-        #print(url)
+        
     return url
 if __name__ == '__main__':
     print(share('email', "This is the title", "Hello World!", "https://www.google.com"))
