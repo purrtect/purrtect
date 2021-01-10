dd73a4881070b7fc5578fa9cd7af5a201158a247
stevenf7 <34409734+stevenf7@users.noreply.github.com>
2021-01-10 00:40:38 -0700

    improved comments

--- share.py
+++ share.py
@@ -1,7 +1,7 @@
-### Takes in website ('facebook' or 'twitter')
-### Subject for email titles (only used  for emails)
-### Message to Pre-populate
-### Link toPre-populate
+### site: Takes in website ('facebook' 'twitter' or 'email')
+### subject: email titles (only used  for emails)
+### message to Pre-populate (only works for twitter and email)
+### Link to Pre-populate
 ### returns URL
 import urllib.parse
 def share(site, subject, message, link):
@@ -14,7 +14,7 @@ def share(site, subject, message, link):
 
     elif(site == 'email'):
         url = 'mailto:?subject=' + urllib.parse.quote(subject)+ '&body='+urllib.parse.quote(message)+"%0A%0A"+urllib.parse.quote(link)
-        
+
     return url
 if __name__ == '__main__':
     print(share('email', "This is the title", "Hello World!", "https://www.google.com"))
